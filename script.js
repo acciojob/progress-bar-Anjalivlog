1ba8112db5545d61c5276612d3cc0ae9c8e331b8
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 14:15:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentCircle = 0;
+
+updateButtons();
+
+prevButton.addEventListener('click', () => {
+    if (currentCircle > 0) {
+        circles[currentCircle].classList.remove('active');
+        currentCircle--;
+        updateButtons();
+    }
+});
+
+nextButton.addEventListener('click', () => {
+    if (currentCircle < circles.length - 1) {
+		currentCircle++;
+        circles[currentCircle].classList.add('active');
+        updateButtons();
+    }
+});
+
+function updateButtons() {
+    if (currentCircle === 0) {
+        prevButton.disabled = true;
+    } else {
+        prevButton.disabled = false;
+    }
+
+    if (currentCircle === circles.length - 1) {
+        nextButton.disabled = true;
+    } else {
+        nextButton.disabled = false;
+    }
+}
